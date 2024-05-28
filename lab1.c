55dccd7c476f7160fc218e03d2f66fc9fa63a1a0
Mikhail Agilov <144871990+eglogger@users.noreply.github.com>
2024-05-28 11:13:31 +0700

    Add files via upload

---
+++ lab1.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+void compressString(char *str) {
+
+    char *p = str;
+    char *q = str;
+    char curr_char = *p;
+    int count = 1;
+
+    while (*p != '\0') {
+        p++;
+
+        if (*p == curr_char) {
+            count++;
+        } else {
+            if (count > 1) {
+                *q++ = count + '0';
+            }
+            *q++ = curr_char;
+            curr_char = *p;
+            count = 1;
+        }
+    }
+    *q = '\0';
+}
+
+int main() {
+
+    char str[] = "aaaaabbccccccddddeeeeeee";
+    printf("Source string: %s\n", str);
+
+    compressString(str);
+
+    printf("Compressed string: %s\n", str);
+
+    return 0;
+}
