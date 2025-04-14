86e9a82f532795c2bccbb306fa2ed2ea0254d045
Michael <144871990+eglogger@users.noreply.github.com>
2025-04-14 17:04:46 +0700

    icon added

--- semester4/simulator/src/main/java/com/example/simulator/Main.java
+++ semester4/simulator/src/main/java/com/example/simulator/Main.java
@@ -2,10 +2,21 @@ package com.example.simulator;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.scene.image.Image;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) {
+        Image icon = null;
+        try {
+            icon = new Image(new FileInputStream("src/main/resources/com/example/simulator/icon.png"));
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        primaryStage.getIcons().add(icon);
         Habitat habitat = Habitat.getInstance(primaryStage);
         primaryStage.setTitle("Simulation");
         primaryStage.show();
--- semester4/simulator/src/main/java/module-info.java
+++ semester4/simulator/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.simulator {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
 
 
     opens com.example.simulator to javafx.fxml;
